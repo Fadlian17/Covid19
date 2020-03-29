84e0208b157e9d6bac7c5fbefcaec5bab12fc9b4
Fadlian17 <alfansyahfadlian@gmail.com>
2020-03-29 19:43:45 +0700

    update Flags

--- app/src/main/java/com/alfansyah/android/covid19/ui/country/CountryFragment.java
+++ app/src/main/java/com/alfansyah/android/covid19/ui/country/CountryFragment.java
@@ -50,7 +50,7 @@ public class CountryFragment extends Fragment {
     }
 
     private void showRecyclerView(){
-        CovidCountryAdapter covidCountryAdapter = new CovidCountryAdapter(covidCountries);
+        CovidCountryAdapter covidCountryAdapter = new CovidCountryAdapter(covidCountries,getActivity());
         rvCovidCountry.setAdapter(covidCountryAdapter);
     }
 
@@ -70,7 +70,20 @@ public class CountryFragment extends Fragment {
                         JSONArray jsonArray = new JSONArray(response);
                         for (int i = 0; i < jsonArray.length(); i++) {
                             JSONObject data = jsonArray.getJSONObject(i);
-                            covidCountries.add(new CovidCountry(data.getString("country"), data.getString("cases")));
+
+                            //extract JSON Object  inside JSON Object
+                            JSONObject countryInfo = data.getJSONObject("countryInfo");
+
+                        covidCountries.add(new CovidCountry(
+                                    data.getString("country"),
+                                    data.getString("cases"),
+                                    data.getString("todayCases"),
+                                    data.getString("deaths"),
+                                    data.getString("todayDeaths"),
+                                    data.getString("recovered"),
+                                    data.getString("active"),
+                                    countryInfo.getString("flag")
+                                    ));
                         }
                         showRecyclerView();
                     } catch (JSONException e) {
